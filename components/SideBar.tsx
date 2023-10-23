55f44ae4b2c17c2a9dc03886ef34a0836d58b084
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-10-23 13:23:02 -0400

    getting a new Session(after user logged in) and SignOut ()

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,16 +1,29 @@
+"use client";
+
 import NewChat from "./NewChat";
+import { useSession, signOut } from "next-auth/react";
 
 function SideBar() {
+  const { data: session } = useSession();
   return (
-    <div>
+    <div className="flex  flex-col p-2 h-screen">
       <div className="flex-1">
-        <div className="flex  flex-col p-2">
+        <div className="">
           {/* New Chat */}
           <NewChat />
           <div>{/* ModelSelection */}</div>
           {/* Chat through the ChatRow */}
         </div>
       </div>
+
+      {session && (
+        <img
+          onClick={() => signOut()}
+          src={session.user?.image!}
+          alt="Profile pic"
+          className="h-12 w-12 rounded-full cursor-pointer mx-auto mb-2 hover:opacity-50"
+        />
+      )}
     </div>
   );
 }
