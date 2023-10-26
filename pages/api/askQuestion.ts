f5bc9800fa64a72dcf0f04f5f2b455f6fe3ffb62
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-10-25 20:14:53 -0400

    setup chatgpt and add the response to Firebase - so far it doesn't work

--- pages/api/askQuestion.ts
+++ pages/api/askQuestion.ts
@@ -1,6 +1,8 @@
-
-import { NextApiRequest, NextApiResponse } from "next"
+import admin from 'firebase-admin'
+import type { NextApiRequest, NextApiResponse } from 'next'
 import query from "@/util/queryApi"
+import { adminDb } from "@/firebaseAdmin"
+
 type Data = {
   answer: string
 }
@@ -8,7 +10,7 @@ type Data = {
 
 expor default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse<Data>
 ) {
   const {prompt, chatId, model, session} = req.body
 
@@ -25,6 +27,24 @@ expor default async function handler(
   //ChatGPT Query
   const response =  await query(prompt, chatId, model)
 
+  const message: Message = {
+    text: response || "ChatGPT was unable to find an answer for that!",
+    createdAt: admin.firestore.Timestamp.now(),
+    user: {
+      _id: "ChatGPT",
+      name: 'ChatGPT',
+      avatar: "https://links.papareact.com/89k"
+    }
+  }
 
-  res.status(200).json({name: 'John Doe'})
+
+  await adminDb
+  .collection('users')
+  .doc(session?.user?.email!)
+  .collection('chats')
+  .doc(chatId)
+  .collection("message")
+  .add(message)
+
+  res.status(200).json({answer: message.text})
 }
